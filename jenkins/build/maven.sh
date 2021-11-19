ea1622dca46b738a7b581c4e97e110aa3c5c409c
EC2 Default User <ec2-user@ip-172-31-9-183.ap-south-1.compute.internal>
2021-11-19 19:54:47 +0000

    Path Modifications made

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -3,4 +3,6 @@ echo "************************"
 echo "** Building Jar ***"
 echo "************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6-alpine "$@"
+WORKSPACE=/home/ec2-user/jenkins/jenkins_data/workspace/Pipeline-Maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6-alpine "$@"
--- jenkins/test/maven.sh
+++ jenkins/test/maven.sh
@@ -2,5 +2,6 @@
 echo "************************"
 echo "** Testing Jar ***"
 echo "************************"
+WORKSPACE=/home/ec2-user/jenkins/jenkins_data/workspace/Pipeline-Maven
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6-alpine "$@"
