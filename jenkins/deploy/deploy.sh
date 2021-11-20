f093ed8279b2f7d33b57d5eec59ba49d35c6e60d
EC2 Default User <ec2-user@ip-172-31-9-183.ap-south-1.compute.internal>
2021-11-20 16:51:11 +0000

    IP address changed

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
-scp -i /opt/deploy /tmp/.auth ec2-user@ec2-52-66-245-51.ap-south-1.compute.amazonaws.com:/tmp/.auth
-scp -i /opt/deploy ./jenkins/deploy/publish ec2-user@ec2-52-66-245-51.ap-south-1.compute.amazonaws.com:/tmp/publish
-ssh -i /opt/deploy ec2-user@ec2-52-66-245-51.ap-south-1.compute.amazonaws.com "/tmp/publish"
+scp -i /opt/deploy /tmp/.auth ec2-user@ec2-65-0-106-223.ap-south-1.compute.amazonaws.com:/tmp/.auth
+scp -i /opt/deploy ./jenkins/deploy/publish ec2-user@ec2-65-0-106-223.ap-south-1.compute.amazonaws.com:/tmp/publish
+ssh -i /opt/deploy ec2-user@ec2-65-0-106-223.ap-south-1.compute.amazonaws.com "/tmp/publish"
